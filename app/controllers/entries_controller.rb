e520641ca2b65e9d6155289ceefabe28b52cd193
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-05-22 04:01:45 +0000

    created form for places entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,15 @@
 class EntriesController < ApplicationController
+
+  def index 
+    @entries = Entry.all 
+  end 
+  
   def new 
     @entry = Entry.new 
   end 
+
+  def create
+    @entry = Entry.new(params["entry"])
+    @entry.save 
+  end
 end
