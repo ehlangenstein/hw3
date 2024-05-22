81c223539de72c0a46febb7e4bc2256de6239927
Elizabeth Langenstein <elizabeth.langenstein@kellogg.northwestern.edu>
2024-05-22 06:06:28 +0000

    hw 3

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,11 +6,13 @@ class EntriesController < ApplicationController
   
   def new 
     @entry = Entry.new 
+    @entry["place_id"]=params["place_id"]
   end 
 
   def create
     # new contact
     @entry = Entry.new
+    
     # assign user-entered form data to entry column
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
